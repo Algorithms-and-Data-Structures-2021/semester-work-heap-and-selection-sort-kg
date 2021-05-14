7ea9efeca01439f11a6de71a4c800fd5d9ea7834
Varvara Korneeva <varvara_korneeva@mail.ru>
2021-05-14 15:18:09 +0300

    heap sort

--- include/data_structure.hpp
+++ include/data_structure.hpp
@@ -16,9 +16,7 @@ namespace itis {
     for (int i = 0; i < data.size()-1; i++){
       for (int j = i + 1; j < data.size(); j++){
         if (data[i] > data[j]) {
-          int x = data[i];
-          data[i] = data[j];
-          data[j] = x;
+          std::swap(data[i], data[j]);
         }
       }
     }
@@ -45,13 +43,13 @@ namespace itis {
       std::swap(heap[0],heap[size_-1]);
       size_--;
       int i = 0;
-      int m = 2*i+1;
-      if (m+1<size_ && heap[m]>heap[m+1]) m++;
-      while (2*i<size_ && heap[i]>heap[m]){
-        std::swap(heap[i],heap[m]);
-        i = m;
-        m = 2*i+1;
-        if (m+1<size_ && heap[m]>heap[m+1]) m++;
+      int leftChild = 2*i+1;
+      if (leftChild+1<size_ && heap[leftChild]>heap[leftChild+1]) leftChild++;
+      while (2*i<size_ && heap[i]>heap[leftChild]){
+        std::swap(heap[i],heap[leftChild]);
+        i = leftChild;
+        leftChild = 2*i+1;
+        if (leftChild+1<size_ && heap[leftChild]>heap[leftChild+1]) leftChild++;
       }
       return min;
     }
