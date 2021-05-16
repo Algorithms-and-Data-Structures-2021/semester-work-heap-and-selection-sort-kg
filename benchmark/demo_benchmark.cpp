079ef3d4aed54184e70b1ccdcf28970109f8a242
Varvara Korneeva <varvara_korneeva@mail.ru>
2021-05-16 19:05:56 +0300

    benchmark

--- benchmark/demo_benchmark.cpp
+++ benchmark/demo_benchmark.cpp
@@ -36,8 +36,8 @@ int main(int argc, char **argv) {
 
   const auto path = string(kDatasetPath);
 
-//  vector<string> mas{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
-  vector<string> mas{"1", "2", "3", "4", "5"};
+  vector<string> mas{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+//  vector<string> mas{"1", "2", "3", "4", "5"};
 //  vector<string> mas2{"100", "1000", "10000", "100000", "200000", "300000", "400000", "500000", "600000", "700000", "800000", "900000", "1000000"};
 //  vector<string> mas2{"100", "1000", "10000", "200000", "400000", "600000", "800000", "1000000"};
   vector<string> mas2{"100", "1000", "10000", "250000", "500000", "750000", "1000000"};
@@ -46,7 +46,7 @@ int main(int argc, char **argv) {
   auto output_file = ofstream(path);
   output_file.open(path + "/" + "result.csv", ios::out | ios::app);
   output_file << "file" << "," << "count" << "," << "number" << "," << "selection_sort" << "," << "heap_sort" << endl;
-  for(int i =0; i < 13; i++){
+  for(int i =0; i < 7; i++){
     for(int j = 0; j < 5; j++){
       for(int k = 0; k < 10; k++){
         auto path_file = string("/selection_sort/" + mas3.at(j) + "/" + mas2.at(i) + ".csv");
@@ -58,8 +58,7 @@ int main(int argc, char **argv) {
         itis::selection_sort(data, n);
         const auto time_point_after = chrono::steady_clock::now();
         const auto time_diff = time_point_after - time_point_before;
-        const long time_elapsed_ns = chrono::duration_cast<chrono::milliseconds>(time_diff).count();
-//        const long time_elapsed_ns = 0;
+        const auto time_elapsed_ns = chrono::duration_cast<chrono::milliseconds>(time_diff).count();
 
         auto path_file2 = string("/heap_sort/" + mas3.at(j) + "/" + mas2.at(i) + ".csv");
         auto data2 = makeArray(path + path_file2);
@@ -67,7 +66,7 @@ int main(int argc, char **argv) {
         main_heap->sort(data2);
         const auto time_point_after2 = chrono::steady_clock::now();
         const auto time_diff2 = time_point_after2 - time_point_before2;
-        const long time_elapsed_ns2 = chrono::duration_cast<chrono::nanoseconds>(time_diff2).count();
+        const long time_elapsed_ns2 = chrono::duration_cast<chrono::microseconds>(time_diff2).count();
 
         output_file << mas3.at(j) << "," << mas2.at(i) << "," << mas.at(k) << "," << time_elapsed_ns << "," << time_elapsed_ns2 << endl;
       }
